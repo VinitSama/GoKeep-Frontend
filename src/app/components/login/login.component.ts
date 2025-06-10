55398863b3734957bcdb2ff5e5a338251f2aaa53
VinitSama <vinitaggarwal08072002@gmail.com>
2025-06-11 00:00:41 +0530

    first commit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -28,8 +28,8 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      email: ['testE', [Validators.required]],
-      password: ['testP', [Validators.required]]
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
     });
   }
 
