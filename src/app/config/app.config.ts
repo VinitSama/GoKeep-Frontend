d2ac9ab20743a7ef20aedf5b23e91826ca4b9919
VinitSama <vinitaggarwal08072002@gmail.com>
2025-06-11 17:49:31 +0530

    render-adress

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -6,7 +6,7 @@ import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/
 import { authInterceptor } from '../interceptors/auth.interceptor';
 
 // const baseUrl = 'https://localhost:5117/';
-const baseUrl = 'https://13.228.225.19:8080/';
+const baseUrl = 'https://13.228.225.19/';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(withFetch(), withInterceptors([authInterceptor])), provideAnimationsAsync()]
