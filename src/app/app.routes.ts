0ee0ca3c276e858b58beb8c42b81fda642fcc178
VinitSama <vinitaggarwal08072002@gmail.com>
2025-06-11 18:32:57 +0530

    rout-adjst

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -19,7 +19,7 @@ export const routes: Routes = [
     {
         path: 'home',
         loadComponent: () => import("./components/home/home.component").then(m => m.HomeComponent),
-        // canActivate: [AuthGaurdService]
+        canActivate: [AuthGaurdService]
     },
     {
         path:'**',
