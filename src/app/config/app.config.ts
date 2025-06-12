414e01244d8248a81bb78289fc410a97abb38165
VinitSama <vinitaggarwal08072002@gmail.com>
2025-06-12 12:43:54 +0530

    reder-url

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -5,8 +5,9 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from '../interceptors/auth.interceptor';
 
-// const baseUrl = 'https://localhost:5117/';
-const baseUrl = 'https://13.228.225.19/';
+// const baseUrl = 'https://localhost:7240/';
+// const baseUrl = 'https://13.228.225.19/';
+const baseUrl = 'https://gokeep-server.onrender.com/';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(withFetch(), withInterceptors([authInterceptor])), provideAnimationsAsync()]
